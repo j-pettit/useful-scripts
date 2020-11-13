eabea6241143a4a2cfe0b9db430973c68d4eb9c8
Julian Pettit <julian.s.pettit@gmail.com>
2020-11-13 13:39:05 -0500

    add argparsing and fix repeating character bug

--- random_pass.py
+++ random_pass.py
@@ -1,6 +1,14 @@
+''' Generate a secure random password'''
+
+import argparse
 import random
 import secrets
 import string
+
+parser = argparse.ArgumentParser(description='generate a secure password')
+parser.add_argument('-l', '--length', metavar='size', type=int, help='the length of the generated password', default=8)
+parser.add_argument('-a', '--allow-repeats', action='store_true', help='allow the password to contain repeated characters')
+args = parser.parse_args()
 
 UPPERCASE_CHARS = tuple(string.ascii_uppercase)
 LOWERCASE_CHARS = tuple(string.ascii_lowercase)
@@ -23,7 +31,7 @@ def generate_random_password(total, sequences):
             n += 1
     random.shuffle(password)
 
-    while _is_repeating(password):
+    while not args.allow_repeats and _is_repeating(password):
         random.shuffle(password)
 
     return ''.join(password)
@@ -44,6 +52,6 @@ def _is_repeating(password):
     for i in range(1, len(password)):
         if password[i] == password[i-1]:
             return True
-        return False
+    return False
 
-print(generate_random_password(random.randint(6, 6), SEQUENCE))
+print(generate_random_password(args.length, SEQUENCE)) 
