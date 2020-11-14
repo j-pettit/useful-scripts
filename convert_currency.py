8c19dc94d89983ee8a9af37171ff080e73dc9363
Julian Pettit <julian.s.pettit@gmail.com>
2020-11-14 13:46:13 -0500

    add convert currency request code

---
+++ convert_currency.py
@@ -0,0 +1,13 @@
+import argparse
+import json
+from urllib import request
+
+parser = argparse.ArgumentParser(description='find the conversion rate between two currencies')
+parser.add_argument('base', help='the currency to convert from')
+parser.add_argument('target', help='the currency to convert to')
+parser.add_argument('-v', '--value', metavar='value', type=float, default=1.0, help='the amount of base currency to be converted')
+args = parser.parse_args()
+
+url = "http://freecurrencyrates.com/api/action.php?do=cvals&iso=" + args.target + "&f=" + args.base + "&v=1&s=cbr"
+r = request.urlopen(url)
+obj = json.loads(r.read())
